c498d30a47f79969ef1ce6ccb10f28854855260f
Abagail Hudson <abagailanne@gmail.com>
2019-05-04 20:04:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiouAEIOU]+\w/)
-    return true
-  else
-    return false
-  end
+  if word.match(/\A[aeiouAEIOU]+\w/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,11 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].+[.!,?]$/)
-    return true
-  else
-    return false
-  end
+  text.match(/^[A-Z].+[.!,?]$/)? true : false
 end
 
 def valid_phone_number?(phone)
